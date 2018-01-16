544dc42e04b6bcb03c003866a211e0f6442da608
Phakama Ntshewula <Phakama@construction-computer-software.com>
2018-01-16 11:38:22 +0200

    Used services and HTTP request to retrieve data from server with observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
   template: `
   <h1>{{title}}</h1>
-  <pm-products></pm-products>`
-  ,
- // templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  <pm-products></pm-products>`,
+  providers: [ProductService]
 })
 export class AppComponent {
   title: string = ' Acme Product Management';
