6d1c9978d7e533b832353b83b57094bb9aa9b696
Phakama Ntshewula <Phakama@construction-computer-software.com>
2018-01-16 14:54:40 +0200

    Switched to using routing to navigate pages

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { IProduct } from './product';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(private _route: ActivatedRoute) { }
+
+  ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`;
+  }
+
+}
